5cee6325e6cb206e7d87c30927fba31cb308a927
YaroslavIonin <91628164+YaroslavIonin@users.noreply.github.com>
2022-08-14 21:03:51 +0500

    added all files

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Navbar />
+    <Slider />
+    <div>
+      <Nuxt />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+
+<script>
+import Navbar from "~/components/Navbar";
+import Slider from "~/components/Slider";
+import Footer from "~/components/Footer";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+    Slider,
+  },
+  head() {
+    return {
+      link: [
+        { rel: "canonical", href: `http://localhost:3000${this.$route.path}`}
+      ]
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
